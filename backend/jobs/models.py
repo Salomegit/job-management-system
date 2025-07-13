66633138a686d7768be30752ac639faa0334d59b
Salomegit <sallygithinji72@gmail.com>
2025-07-13 20:38:46 +0300

    Initialize Django project structure with basic settings, models, and configurations

---
+++ backend/jobs/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+
+
+class  Job(models.Model):
+    STATUS_CHOICES = [
+         ('active', 'Active'),
+        ('inactive', 'Inactive')],
+    title = models.CharField(max_length=255, null=False, blank=False),
+    description = models.TextField( null=True, blank=True),
+    company_name = models.CharField(max_length=200, null=False, blank=False),
+    location = models.CharField(max_length=200,  null=False, blank=False),
+    salary = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False, validators=[MinValueValidator(0)]),
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active'),
+    created_at = models.DateTimeField(auto_now_add=True),
+    updated_at = models.DateTimeField(auto_now=True),
+    
+class Meta:
+        verbose_name = 'Job'
+        verbose_name_plural = 'Jobs'
+        ordering = ['-created_at']
+        
+def __str__(self):
+    return f"{self.title} at {self.company_name}"
+    
+def soft_delete(self):
+    self.status = 'inactive'
+    self.save()
+    
+# Create your models here.
